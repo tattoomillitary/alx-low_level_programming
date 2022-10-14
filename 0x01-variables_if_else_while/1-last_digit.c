b6f546704aec7e60a964006319a8fb8a58b1acb6
tattoomillitary <successbraimoh@gmail.com>
2022-10-14 06:02:34 -0700

    Yes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (l == o)
+	else if (l == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
