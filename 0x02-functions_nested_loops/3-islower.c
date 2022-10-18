d79d14f58a95b68562c20905b7805bf3201901eb
tattoomillitary <successbraimoh@gmail.com>
2022-10-18 06:35:07 -0700

    fixed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,23 @@
 
 /**
  * _islower - checks if the character is lowercase
+ * 0
+ *
  * @C: a character argument
  *
  * Return: 1 for lowercase character. 0 for the rest.
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c <== 'z');
+	int c;
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
