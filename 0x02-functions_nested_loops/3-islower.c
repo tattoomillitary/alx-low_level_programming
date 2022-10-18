99defe14ef2be8db322800db301a0ba0a4e4a08d
tattoomillitary <successbraimoh@gmail.com>
2022-10-18 06:10:35 -0700

    another one

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower -checks if the character is lowercase
+ * _islower - checks if the character is lowercase
  * @C: a character argument
  *
  * Return: 1 for lowercase character. 0 for the rest.
