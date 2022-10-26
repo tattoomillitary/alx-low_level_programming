fdbe67213f3e81c255d8218e347c945f837c221a
tattoomillitary <successbraimoh@gmail.com>
2022-10-26 13:13:34 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * string_toupper - changes all owercase letters of a string
+ * string_toupper - changes all lowercase letters of a string
  * to uppercase
  * @s: input string.
  * Return: the pointer to dest.
